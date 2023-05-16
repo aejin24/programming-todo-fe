3e679169537c692b98b071c6a04df076bf7bd53d
aejin24 <aejin24@naver.com>
2023-05-16 17:56:30 +0900

    add auth page ui

--- src/pages/login/Auth.tsx
+++ src/pages/login/Auth.tsx
@@ -1,3 +1,11 @@
+import styles from "assets/scss/pages/login/auth.module.scss";
+
 export default function Auth() {
-  return <>Progressing...</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles["dot-falling"]} />
+      <p className={styles.text}>로그인 진행 중</p>
+      <p className={styles.subtext}>이 화면이 지속되면 F5를 눌러주세요.</p>
+    </div>
+  );
 }
